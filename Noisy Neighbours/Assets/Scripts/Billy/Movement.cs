071f70e28af7d200e06c4a1624e253a68f4443c2
OmarOH <36475683+OmarOH@users.noreply.github.com>
2020-01-15 17:21:23 +0100

    Basic movement/camer follow/first sprites

---
+++ Noisy Neighbours/Assets/Scripts/Billy/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D character;
+    Vector2 movement;
+
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+
+    void FixedUpdate()
+    {
+        // Movement
+        character.MovePosition(character.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}

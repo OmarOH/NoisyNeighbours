e84b15e95f24c07248d78b6a5498758f8be9a75e
OmarOH <36475683+OmarOH@users.noreply.github.com>
2020-08-19 22:46:08 +0200

    Minor animation fixes
    Fixed minor animation issues

--- Noisy Neighbours/Assets/Scripts/Objects/InteractiveObject.cs
+++ Noisy Neighbours/Assets/Scripts/Objects/InteractiveObject.cs
@@ -1,10 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class InteractiveObject : MonoBehaviour
 {
-    protected float raduis;
-    protected float state;
-    protected float timer;
+    //public ObjectsData obj;
+    public float radius;
+    public float timer;
+    public float countdown;
+    public bool triggered = false;
+    public bool onState;
+    public SpriteRenderer idleSpr;
+    public SpriteRenderer activeSpr;
+    public Text text;
+
+    private void Start()
+    {
+        //radius = obj.radius;
+        //timer = obj.timer;
+        //idleSpr = obj.idleSprite;
+        //activeSpr = obj.activeSprite;
+        //idleSpr = GetComponent<SpriteRenderer>();
+        //activeSpr = GetComponent<SpriteRenderer>();
+        text = text.GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space") && triggered)
+        {
+            if (onState)
+            {
+                Debug.Log("registered");
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        triggered = false;   
+    }
+
+    void TurnOn()
+    {
+        onState = true;
+        text.text = timer.ToString();
+        gameObject.GetComponent<SpriteRenderer>().sprite = activeSpr.sprite;
+    }
+
+    void TurnOff()
+    {
+        onState = false;
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = idleSpr.sprite;
+    }
+
+    void CountdownTimer(float time)
+    {
+
+    }
 }
---
+++ Noisy Neighbours/Assets/Scripts/Objects/ObjectsData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Interactive Object", order = 1)]
+public class ObjectsData : ScriptableObject
+{
+    public float timer;
+    public float radius;
+    public AudioSource sound;
+    public SpriteRenderer idleSprite;
+    public SpriteRenderer activeSprite;
+}

f4e25b8337d9e91afddc6e5b31e8406cf413c0ca
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-05-18 03:15:30 +0200

    Groundwork done
    - Basic movement
    - Basic animation

--- Noisy Neighbours/Assets/Scripts/Player/Movement.cs
+++ Noisy Neighbours/Assets/Scripts/Player/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour
     private Rigidbody2D character;
     private Animator animator;
     private Vector2 movement;
+    private Transform grabPoint;
 
     private void Awake()
     {
         character = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        grabPoint = transform.Find("GrabRange");
     }
 
     private void Update()
@@ -24,6 +26,19 @@ public class Movement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // Get the last direction input so the player faces the last direction of input when idle
+        if(movement.x == 1 || movement.x == -1 
+            || movement.y == 1 || movement.y == -1)
+        {
+            animator.SetFloat("LastHorizontal", movement.x);
+            animator.SetFloat("LastVertical", movement.y);
+        }
+
+        if (animator.GetFloat("LastHorizontal") > 0) grabPoint.localRotation = Quaternion.Euler(0, 0, 90);
+        if (animator.GetFloat("LastHorizontal") < 0) grabPoint.localRotation = Quaternion.Euler(0, 0, -90);
+        if (animator.GetFloat("LastVertical") > 0) grabPoint.localRotation = Quaternion.Euler(0, 0, 180);
+        if (animator.GetFloat("LastVertical") < 0) grabPoint.localRotation = Quaternion.Euler(0, 0, 0);
     }
 
 
